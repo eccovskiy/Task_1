d2e46d5c7fbd78040d2e20348e5500c4412367ca
eccovskiy17 <eccovskiy17@gmail.com>
2019-08-12 15:57:34 +0300

    12 august

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 'use srtict';
 
-var money = prompt("Ваш бюджет на месяц, ''");
-var time = prompt("Введите дату в формате YYYY-MM-DD, ''");
+let money = +prompt("Ваш бюджет на месяц, ''");
+let time = prompt("Введите дату в формате YYYY-MM-DD, ''");
 
-var appData = {
+let appData = {
     budget: money,
     timeData: time,
     expenses: {},
@@ -12,15 +12,36 @@ var appData = {
     savings: false
 };
 
-var ans1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    ans2 = prompt("Во сколько обойдется?", ''),
-	ans3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	ans4 = prompt("Во сколько обойдется?", '');
+	// ans3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	// ans4 = prompt("Во сколько обойдется?", '');
 
-appData.expenses.ans1 = ans2;
-appData.expenses.ans3 = ans4;
+// appData.expenses.ans1 = ans2;
+// appData.expenses.ans3 = ans4;
 
 console.log(appData);
 
+for (let i = 0; i < 2; i++){
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
 
-alert(appData.budget / 30);
+    if ( (typeof(a)==='string') && (typeof(a) != null ) && (typeof(b) != null && a != '' && b != '' && a.length < 50) ) {
+        console.log("Done!");
+        appData.expenses[a] = b;
+    } else {
+
+    }
+};
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Daily budget: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100){
+    console.log("Minimal");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Medium");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("High");
+} else {
+    console.log("Error");
+}
